00711d6a30a4c4c365db362a2f0c8879a9d4a3ba
José David Moreno Posada <josdavidmo@gmail.com>
2018-08-16 07:51:12 -0500

    add list and sign_in templates

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ import selector
 from users import view
 
 urls = selector.Selector()
+urls.add('/', GET=view.sign_in_user_get)
+urls.add('/', POST=view.sign_in_user_post)
 urls.add('/list/', GET=view.list_get)
 urls.add('/list/', POST=view.list_post)
--- users/view.py
+++ users/view.py
@@ -9,6 +9,33 @@ from dbconfig import engine
 from users.model import User
 
 
+def sign_in_user_get(environ, start_response):
+    usr_session = environ['beaker.session']
+    usr_session.delete()
+    return render(start_response, 'sign_in.html')
+
+
+def sign_in_user_post(environ, start_response):
+    import hashlib
+    request = POST(environ)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    password = hashlib.sha256(request['password'].encode()).hexdigest()
+    query = session.query(User).filter(
+        User.email == request['email'], User.password == password)
+    exists = query.count() == 1
+    if exists:
+        usr = query[0]
+        usr_session = environ['beaker.session']
+        usr_session['usr_id'] = usr.id
+        usr_session.save()
+        return redirect(start_response, '/list/')
+    else:
+        return redirect(start_response, '/')
+
+
 def list_get(environ, start_response):
     usr_session = environ['beaker.session']
     if usr_session.get('usr_id'):
